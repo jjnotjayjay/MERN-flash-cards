e34a6ab55c6f22a927b393c0af265323033e86e7
jjnotjayjay <jj@alumni.usc.edu>
2018-09-24 09:53:12 -0700

    Made form visually appealing by styling with Bootstrap.

--- createcard.js
+++ createcard.js
@@ -7,21 +7,21 @@ export default class CreateCard extends React.Component {
 
   render() {
     return (
-      <div>
-        <h4>Create a Flash Card</h4>
-        <div>
+      <div className="col-4 offset-md-4 pt-3">
+        <h4 className="text-center">Create a Flash Card</h4>
+        <div className="form-group">
           <label for="topicInput">Topic: </label>
-          <input type="text" id="topicInput"></input>
+          <input className="form-control" id="topicInput" type="text"></input>
         </div>
-        <div>
+        <div className="form-group">
           <label for="sideAInput">Side A: </label>
-          <input type="text" id="sideAInput"></input>
+          <input className="form-control" id="sideAInput" type="text"></input>
         </div>
-        <div>
+        <div className="form-group">
           <label for="sideBInput">Side B: </label>
-          <textarea type="text" id="sideBInput"></textarea>
+          <textarea className="form-control" id="sideBInput" type="text" rows="4"></textarea>
         </div>
-        <button type="submit" id="submitCard">Create Card</button>
+        <button className="btn btn-primary" id="submitCard" type="submit">Create Card</button>
       </div>
     )
   }
