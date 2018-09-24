f222fbfe246c773439ccfd154c3ed1a27ae27cdc
jjnotjayjay <jj@alumni.usc.edu>
2018-09-24 10:19:42 -0700

    Link input boxes to form state so user can input text. #1

--- createcard.js
+++ createcard.js
@@ -3,23 +3,54 @@ import React from 'react'
 export default class CreateCard extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      topic: '',
+      sideA: '',
+      sideB: ''
+    }
+  }
+
+  handleChange(e, id) {
+    this.setState({
+      [id]: e.target.value
+    })
   }
 
   render() {
+    const {topic, sideA, sideB} = this.state
     return (
       <div className="col-4 offset-md-4 pt-3">
         <h4 className="text-center">Create a Flash Card</h4>
         <div className="form-group">
-          <label for="topicInput">Topic: </label>
-          <input className="form-control" id="topicInput" type="text"></input>
+          <label>Topic: </label>
+          <input
+            className="form-control"
+            id="topicInput"
+            type="text"
+            value={topic}
+            onChange={(e) => this.handleChange(e, 'topic')}>
+          </input>
         </div>
         <div className="form-group">
-          <label for="sideAInput">Side A: </label>
-          <input className="form-control" id="sideAInput" type="text"></input>
+          <label>Side A: </label>
+          <input
+            className="form-control"
+            id="sideAInput"
+            type="text"
+            value={sideA}
+            onChange={(e) => this.handleChange(e, 'sideA')}>
+          </input>
         </div>
         <div className="form-group">
-          <label for="sideBInput">Side B: </label>
-          <textarea className="form-control" id="sideBInput" type="text" rows="4"></textarea>
+          <label>Side B: </label>
+          <textarea
+            className="form-control"
+            id="sideBInput"
+            type="text"
+            rows="4"
+            value={sideB}
+            onChange={(e) => this.handleChange(e, 'sideB')}>
+          </textarea>
         </div>
         <button className="btn btn-primary" id="submitCard" type="submit">Create Card</button>
       </div>
