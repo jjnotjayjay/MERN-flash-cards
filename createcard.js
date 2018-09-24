3225ed90f88757419b687a151f161d2568030699
jjnotjayjay <jj@alumni.usc.edu>
2018-09-24 09:40:59 -0700

    Built static version of form with input boxes for Topic, Side A, and Side B. #1

--- createcard.js
+++ createcard.js
@@ -1,1 +1,28 @@
-console.log('hello React')
+import React from 'react'
+
+export default class CreateCard extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Create a Flash Card</h4>
+        <div>
+          <label for="topicInput">Topic: </label>
+          <input type="text" id="topicInput"></input>
+        </div>
+        <div>
+          <label for="sideAInput">Side A: </label>
+          <input type="text" id="sideAInput"></input>
+        </div>
+        <div>
+          <label for="sideBInput">Side B: </label>
+          <textarea type="text" id="sideBInput"></textarea>
+        </div>
+        <button type="submit" id="submitCard">Create Card</button>
+      </div>
+    )
+  }
+}
