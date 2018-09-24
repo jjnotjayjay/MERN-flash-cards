10822e0edfeee297cf6ce9de9b6c4099580f9a27
jjnotjayjay <jj@alumni.usc.edu>
2018-09-24 15:21:54 -0700

    Styled the list using Bootstrap to make it visually appealing. #3

--- viewcards.js
+++ viewcards.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 export default function ViewCards(props) {
   return (
-    <div>
+    <div className="col-6 offset-md-3">
       {props.cards.map((card, index) => {
         return (
-          <div key={index}>
-            <h6>{card.topic}</h6>
-            <h5>{card.sideA}</h5>
-            <p>{card.sideB}</p>
+          <div className="card mt-2" key={index}>
+            <div className="card-body">
+              <p className="card-subtitle">{card.topic}</p>
+              <h5 className="card-title text-center">{card.sideA}</h5>
+              <hr/>
+              <p className="card-text">{card.sideB}</p>
+            </div>
           </div>
         )
       })}
