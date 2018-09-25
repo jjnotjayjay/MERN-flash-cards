e340057006e3436d1e0f4fc3257434566665dc2e
jjnotjayjay <jj@alumni.usc.edu>
2018-09-25 10:21:30 -0700

    Restyled card to make layout more natural and place more prominence on Side A than Topic.

--- viewcards.js
+++ viewcards.js
@@ -15,10 +15,10 @@ export default function ViewCards(props) {
             return (
               <div className="card mt-2" key={index}>
                 <div className="card-body">
-                  <p className="card-subtitle">{card.topic}</p>
-                  <h5 className="card-title text-center">{card.sideA}</h5>
+                  <p className="card-subtitle text-muted text-right font-italic">{card.topic}</p>
+                  <h4 className="card-title text-center">{card.sideA}</h4>
                   <hr/>
-                  <p className="card-text">{card.sideB}</p>
+                  <p className="card-text text-center">{card.sideB}</p>
                 </div>
               </div>
             )
