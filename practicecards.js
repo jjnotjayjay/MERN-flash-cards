3be385e7a7c02c17e955dcbd721839bd53f3a04d
jjnotjayjay <jj@alumni.usc.edu>
2018-09-26 11:17:48 -0700

    'PracticeCards' initializes a currentCard state to 0 to hold the user's current selection. #13

--- practicecards.js
+++ practicecards.js
@@ -3,11 +3,16 @@ import React from 'react'
 export default class PracticeCards extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      currentCard: 0
+    }
   }
 
   render() {
+    const { currentCard } = this.state
+    const { cards } = this.props
     return (
-      <div>foo</div>
+      <div>{cards[currentCard].topic}</div>
     )
   }
 }
