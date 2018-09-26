12ff85f3086bec7f6d5f0f8ac610eb9e9bd483a0
jjnotjayjay <jj@alumni.usc.edu>
2018-09-26 15:18:40 -0700

    Replaced references to the 'nav-link' class with original class identifiers. #13

--- navbar.js
+++ navbar.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function NavBar(props) {
   return (
     <div className="nav">
-      <span className="nav-link" onClick={() => props.updateView('view')}>View Cards</span>
-      <span className="nav-link" onClick={() => props.updateView('create')}>Create Cards</span>
-      <span className="nav-link" onClick={() => props.updateView('practice')}>Practice</span>
+      <span className="nav-link nav-pointer" onClick={() => props.updateView('view')}>View Cards</span>
+      <span className="nav-link nav-pointer" onClick={() => props.updateView('create')}>Create Cards</span>
+      <span className="nav-link nav-pointer" onClick={() => props.updateView('practice')}>Practice</span>
     </div>
   )
 }
