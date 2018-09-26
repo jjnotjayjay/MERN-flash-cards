410d5bdfca0623cba68370472b62138da1cf481f
jjnotjayjay <jj@alumni.usc.edu>
2018-09-26 10:39:40 -0700

    Added link to NavBar for 'Practice' #13

--- navbar.js
+++ navbar.js
@@ -5,6 +5,7 @@ export default function NavBar(props) {
     <div className="nav">
       <span className="nav-link" onClick={() => props.updateView('view')}>View Cards</span>
       <span className="nav-link" onClick={() => props.updateView('create')}>Create Cards</span>
+      <span className="nav-link">Practice</span>
     </div>
   )
 }
