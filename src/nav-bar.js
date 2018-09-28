b4f31b2a55ff2aac7d06f62802b2a5c1f5ba2578
jjnotjayjay <jj@alumni.usc.edu>
2018-09-28 15:01:32 -0700

    Used the Bootstrap Navbar component for navigation. #19

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function NavBar(props) {
   return (
-    <div className="nav">
-      <span className="nav-link nav-pointer" onClick={() => props.updateView('view')}>View Cards</span>
-      <span className="nav-link nav-pointer" onClick={() => props.updateView('create')}>Create Cards</span>
-      <span className="nav-link nav-pointer" onClick={() => props.updateView('practice')}>Practice</span>
+    <div className="navbar bg-light">
+      <div className="nav-item">
+        <span className="nav-link nav-pointer" onClick={() => props.updateView('view')}>View Cards</span>
+      </div>
+      <div className="nav-item">
+        <span className="nav-link nav-pointer" onClick={() => props.updateView('create')}>Create Cards</span>
+      </div>
+      <div className="nav-item mr-auto">
+        <span className="nav-link nav-pointer" onClick={() => props.updateView('practice')}>Practice</span>
+      </div>
     </div>
   )
 }
