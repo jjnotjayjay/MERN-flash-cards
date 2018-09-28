ec97820cb7d0d0ff2006fbe4ef40132d65aad769
jjnotjayjay <jj@alumni.usc.edu>
2018-09-28 15:34:38 -0700

    Added a background image to the app and adjusted card-form and shadows accordingly. #19

--- src/practice-cards.js
+++ src/practice-cards.js
@@ -47,7 +47,7 @@ export default class PracticeCards extends React.Component {
     return (
       <div className="col-10 offset-1 col-sm-8 offset-sm-2 col-lg-6 offset-lg-3 d-flex flex-wrap align-content-center full-height">
         <i className="fas fa-chevron-left fa-2x left-arrow pointer" onClick={() => this.changeCard('left')}></i>
-        <div className="card w-100 card-height shadow-sm">
+        <div className="card w-100 card-height shadow">
           <div className="card-body">
             <p className="text-muted card-subtitle text-right font-italic">{cardToRender.topic}</p>
             <h4 className="card-title mt-1 mb-3">{cardToRender.sideA}</h4>
@@ -59,7 +59,7 @@ export default class PracticeCards extends React.Component {
           </div>
         </div>
         <i className="fas fa-chevron-right fa-2x pointer right-arrow" onClick={() => this.changeCard('right')}></i>
-        <div className="progress w-100 mt-3">
+        <div className="progress w-100 mt-3 progress-color">
           <div className="progress-bar" role="progressbar" style={{width: percentageComplete + '%'}}></div>
         </div>
       </div>
